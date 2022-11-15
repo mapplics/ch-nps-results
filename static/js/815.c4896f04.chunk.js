ab857a21aaf5ce7310c77b16c20e986ea4451736
Santiago Garcia <santiagogarcia97@outlook.com>
2022-11-15 09:26:54 -0300

    Updates

---
+++ static/js/815.c4896f04.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkch_nps_results=self.webpackChunkch_nps_results||[]).push([[815],{8815:function(s,e,u){u.r(e);var c=u(184);e.default=function(){return(0,c.jsx)("div",{className:"w-48 h-48 bg-black"})}}}]);
+//# sourceMappingURL=815.c4896f04.chunk.js.map
